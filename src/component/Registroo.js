9aaa324621c9ca950888e3dba2a8578cb44f2583
Andres Florez <anflo.97@gmail.com>
2019-08-31 17:02:16 -0500

    adicion logo

--- src/component/Registroo.js
+++ src/component/Registroo.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
+//import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+//import FormControlLabel from '@material-ui/core/FormControlLabel';
+//import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 //import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+//import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-
 import Container from '@material-ui/core/Container';
 import "../css/style_registry.css"
+import logo from "../images/logo.jpg"
 class Registroo extends React.Component {
 
     render() {
@@ -21,12 +21,20 @@ class Registroo extends React.Component {
             <Container component="main" maxWidth="xs">
                 <CssBaseline />
                 <div className="paper">
-                    <Avatar>
+                    {/*<Avatar>
                         <LockOutlinedIcon />
-                    </Avatar>
+                    </Avatar>*/}
+                    <center>
+                        <img className="image" src={logo} alt="Icon Easy Eat"/>
+                    
                     <Typography component="h1" variant="h5">
-                        Sign up
-        </Typography>
+                        Register
+                        
+                    </Typography>
+                    </center>
+                    
+                    <br/>
+        
                     <form className="form" noValidate>
                         <Grid container spacing={2}>
                             <Grid item xs={12} sm={6}>
@@ -75,13 +83,14 @@ class Registroo extends React.Component {
                                     autoComplete="current-password"
                                 />
                             </Grid>
-                            <Grid item xs={12}>
+                            {/*<Grid item xs={12}>
                                 <FormControlLabel
                                     control={<Checkbox value="allowExtraEmails" color="primary" />}
                                     label="I want to receive inspiration, marketing promotions and updates via email."
                                 />
-                            </Grid>
+                            </Grid>*/}
                         </Grid>
+                        <br/><br/>
                         <Button
                             type="submit"
                             fullWidth
